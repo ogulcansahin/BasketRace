6131dec697fb46bb50739590a237be8be8e41bb0
onrkanit3 <onrkanit3@gmail.com>
2021-04-23 00:42:44 +0300

    LastVersion of Prefabs (Basketball Hoop, Ball, Opened and Closed Door, Animations)

---
+++ BasketRace/Assets/Scripts/BasketDetector.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasketDetector : MonoBehaviour
+{
+    
+    
+    startdooranim TheScript;
+    GameObject doorcontrol;
+    private void Start()
+    {
+        doorcontrol = GameObject.FindGameObjectWithTag("doordetector");
+        TheScript = doorcontrol.GetComponent<startdooranim>();
+        
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (TheScript.actualDoorStatus == false)    //Baþlangýçta kapý kapalýysa ve OnTriggerdan dolayý basket olduðunu anlýyor, basket olduysa giriyor.
+        {
+            //Kapýnýn açýlýp kapanmasýna dair iki farklý transition condition var. Bunlar OpenOrClose=True ya da false. Stop ise baþlangýçtaki boþ stateten diðer 
+            //statelere gidip animasyon çalýþarak baþlamasýn diye. Bir kez basket olduktan sonra artýk stop tekrar 0 olamýyor. NotEquals bir kez olsun saðlanýyor 
+            //ve artýk stopun önemi kalmýyor.
+            TheScript.LeftDoor.SetBool("OpenOrClose", true);
+            TheScript.RightDoor.SetBool("OpenOrClose", true);
+            TheScript.actualDoorStatus = true;
+            TheScript.LeftDoor.SetInteger("Stop", 1);
+            TheScript.RightDoor.SetInteger("Stop", 1);
+        }
+
+        else if(TheScript.actualDoorStatus == true) //Baþlangýçta kapý açýksa. 
+        {
+            
+            TheScript.LeftDoor.SetBool("OpenOrClose", false);
+            TheScript.RightDoor.SetBool("OpenOrClose", false);
+            TheScript.actualDoorStatus = false;
+            TheScript.LeftDoor.SetInteger("Stop", 1);
+            TheScript.RightDoor.SetInteger("Stop", 1);
+        }
+
+
+
+        }
+}
---
+++ BasketRace/Assets/startdooranim.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class startdooranim : MonoBehaviour
+{
+    public bool actualDoorStatus; 
+    //Böyle boþ bir script olmasýnýn amacý bu scripti kapýya atýyoruz. Çünkü prefabtan açýk ve kapalý kapý yapacaðýz. Ýçerisinde direkt kayýtlý olmasý için.
+    public Animator LeftDoor; //Unity tarafýndan elle verildi.
+    public Animator RightDoor;
+
+    
+    void Start()
+    {
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+
+        
+    }
+}
