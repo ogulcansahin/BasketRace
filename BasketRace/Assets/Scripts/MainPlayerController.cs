28a26f764c87534f0e1180ce794b2ef2a680859a
onrkanit3 <onrkanit3@gmail.com>
2021-05-06 17:35:51 +0300

    Selam

--- BasketRace/Assets/Scripts/MainPlayerController.cs
+++ BasketRace/Assets/Scripts/MainPlayerController.cs
@@ -4,17 +4,28 @@ using UnityEngine;
 
 public class MainPlayerController : MonoBehaviour
 {
-    GameObject mainPlayer;
+    GameObject mainPlayer; //Karakteri aldýk.
     bool IsRunning = true;
+    private GameObject road; //Flooru aldýk.
+    float road_x;
     void Start()
     {
         mainPlayer = GameObject.FindWithTag("MainPlayer");
+        road = GameObject.FindWithTag("MainFloor");
         
     }
 
     // Update is called once per frame
-    private void FixedUpdate()
+    private void Update()
     {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            ChangeLine(); 
+            
+
+        }
+        
+
         if (IsRunning)
         {
             run();
@@ -30,4 +41,24 @@ public class MainPlayerController : MonoBehaviour
         {
             transform.Translate(transform.forward * Time.deltaTime * 0.8f);
         }
+
+    private void ChangeLine()
+    {
+
+        road_x = (road.transform.position - (road.transform.localScale * 0.215f)).x;
+        StartCoroutine(LaneChangingRouitne());
+
+    }
+
+    IEnumerator LaneChangingRouitne()
+    {
+        
+        while (transform.position.x > road_x)
+        {
+            
+            transform.Translate(transform.right * -1 * Time.deltaTime * 2.9f);
+            yield return new WaitForSeconds(0.005f);
+        }
+    }
 }
+
