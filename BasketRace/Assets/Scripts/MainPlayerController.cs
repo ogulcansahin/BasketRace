fe6b9633981ecd82da464f6a946a4548e5e07aff
Rendall2 <ogulcan16.99@gmail.com>
2021-05-26 14:42:02 +0300

    hizlandirici yavaslatici bir kismi tamamlandi

--- BasketRace/Assets/Scripts/MainPlayerController.cs
+++ BasketRace/Assets/Scripts/MainPlayerController.cs
@@ -20,6 +20,7 @@ public class MainPlayerController : MonoBehaviour
     private Animator [] MainPlayerAnimator;
     private int dieCondition = 0; // Die condition sürekli triggerlanmasýn diye.
     private ParticleSystem [] powerUpRunParticle;
+    float speed = 0.5f;
 
     void Start()
     {
@@ -61,7 +62,7 @@ public class MainPlayerController : MonoBehaviour
     }
     private void run()
         {
-            transform.Translate(transform.forward * Time.deltaTime * 0.5f);
+            transform.Translate(transform.forward * Time.deltaTime * speed);
         }
 
     private void ChangeLine()
@@ -97,6 +98,21 @@ public class MainPlayerController : MonoBehaviour
     }
     private void OnCollisionEnter(Collision collision)
     {
+        if (collision.gameObject.tag == "Fastener")
+        {
+            speed = 1.0f;
+        }
+
+        else if (collision.gameObject.tag == "Slower")
+        {
+            speed = .25f;
+        }
+
+        else if (collision.gameObject.tag == "MainPlayerFloor")
+        {
+            speed = .5f;
+        }
+
         if (collision.gameObject.tag == "Basketball_Skill")
         {
             gameManager.updateBallCount(1);
